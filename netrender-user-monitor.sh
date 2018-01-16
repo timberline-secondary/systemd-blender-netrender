23e4edcac33e57b283625845e547558f533f2158
tylerecouture <tylerecouture@users.noreply.github.com>
2018-01-16 15:57:45 -0800

    Update netrender-user-monitor.sh

--- netrender-user-monitor.sh
+++ netrender-user-monitor.sh
@@ -2,11 +2,12 @@
 
 while true
 do
-  # If Xorg is running, then a user has logged in to the GUI.  `pidof Xorg` Works for Ubuntu's X server. 
-  # Might need to change for other distros. A more universal method of checking would be nice.
-  if pidof Xorg > /dev/null 2>&1; then
-    echo "X window server found.  User has logged in to GUI. I'm outta here!"
+  # Check if a user is logged on locally (via tty),  Will ignore ssh connections.
+  # Might need to change for other distros. 
+  if who | grep tty  > /dev/null 2>&1; then
+    echo "User has logged in locally. I'm outta here!"
     exit 0
   fi
   sleep 1
 done
+
